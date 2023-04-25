9a996a5db785c6c5556e6e83dc128750237684cc
Simon Boggild Hansen <simhan@clin.au.dk>
2023-04-25 15:38:19 +0200

    Comitting all files for the first time

---
+++ R/learning.R
@@ -0,0 +1,16 @@
+# Basics of R -------------------------------------------------------------
+
+colnames(airquality)
+str(airquality)
+summary(airquality)
+
+1 + 1
+
+mean(2:6)
+
+mean(2:6)
+
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
